cf69a9c3ee159198225e7c818fe2cb31c750da42
Medina, Jaiden F <jfmedina@iu.edu>
2025-02-28 16:14:16 -0500

    Deleted extra myFirstApp

--- myFirstApp/src/readfile.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import jsonify, render_template, request, redirect, url_for
-from flask import send_file
-
-def txtfile(filename):
-    with open(filename) as readfile:
-     words = readfile.read().split()
-    return words
-
