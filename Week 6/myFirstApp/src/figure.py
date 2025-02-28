fa3a1c5ab29299dd04ed52c2034380fcb6166e27
Jaiden Medina <jfmedina@iu.edu>
2025-02-28 19:32:11 +0000

    YESS

--- Week 6/myFirstApp/src/figure.py
+++ Week 6/myFirstApp/src/figure.py
@@ -8,6 +8,8 @@ import os
 import io
 from flask import request, jsonify
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 
 UPLOAD_FOLDER='.'
 
@@ -17,19 +19,30 @@ def upload(filename):
     return filename
 
 def gen_plot(name):
-    my_model = load('rfr.pkl')
-    name = request.args.get(name)
+    #test_data = pd.read_csv(name)#, index_col=[0])
+    #fig = test_data.plot(kind='hist').get_figure()
+    #bytes_image = io.BytesIO()
+    #fig.savefig(bytes_image, format = 'png')
+    #bytes_image.seek(0)
+    #return bytes_image
+    try:
+        #my_model = load('rfr.pkl')
+        name = request.args.get('name')
+        if not name:
+            return jsonify({"error": "Invalid Input"}), 405
     # HARD CODED test in here so when I upload I must name my file test
-    test_data = pd.read_csv(name, index_col= False)
-    test_data.plot(kind='hist').get_figure()
-    bytes_image = io.BytesIO()
-    plt.savefig(bytes_image, format = 'png')
-    bytes_image.seek(0)
-    return bytes_image
+        test_data = pd.read_csv(name, index_col= False)
+        test_data.plot(kind='hist').get_figure()
+        bytes_image = io.BytesIO()
+        plt.savefig(bytes_image, format = 'png')
+        bytes_image.seek(0)
+        return bytes_image
+    except KeyError as e:
+        return jsonify({"error": str(e)}), 400
 
 def disp_plot(name):
     plot = gen_plot(name)
-    plot.savefig(os.path.join('static', 'images', 'plot_test.png'))
+    #plot.savefig(os.path.join('static', 'images', 'plot_test.png'))
     #return render_template('test.html')
     return send_file(plot, mimetype='image/png')
 
